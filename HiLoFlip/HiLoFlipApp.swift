3c3a59d127ec8c38ec7337ab1b52523229d7ca88
isaacluke <isaacluke@cedarville.edu>
2026-09-09 15:41:01 -0400

    Initial Commit

---
+++ HiLoFlip/HiLoFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiLoFlipApp.swift
+//  HiLoFlip
+//
+//  Created by Isaac Luke on 9/9/26.
+//
+
+import SwiftUI
+
+@main
+struct HiLoFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
